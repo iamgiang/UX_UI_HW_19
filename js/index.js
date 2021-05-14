85f0b60c8d34562a750d0f05d854af1401f8126f
Giang <giang.nguyen@carsales.com.au>
2021-05-14 20:18:19 +1000

    Attempting to fix navigation

---
+++ js/index.js
@@ -0,0 +1,14 @@
+/* Test to make sure your JavaScript file is loaded correctly using console.log 
+console.log("Testing index.js file is loading correctly");*/
+
+
+/* jQuery Event Listener That Triggers 
+$(document).ready(function(){
+    $("p").hover(function(){
+      $(this).css("background-color", "yellow");
+      }, function(){
+      $(this).css("background-color", "pink");
+    });
+  });*/
+
+  
